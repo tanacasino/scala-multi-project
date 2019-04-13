04f00a459473f3098cd94df6f05ff27e31407550
Tomofumi Tanaka <tanacasino@gmail.com>
2019-04-13 22:53:10 +0900

    Add SimpleRenderer

---
+++ core/src/main/scala/com/github/tanacasino/example/core/SimpleJsonRenderer.scala
@@ -0,0 +1,80 @@
+package com.github.tanacasino.example.core
+
+object SimpleJsonRenderer {
+
+  final def render(json: Json): String = {
+    val sb = new StringBuilder
+    render(json, sb)
+    sb.toString()
+  }
+
+  final def render(json: Json, sb: StringBuilder): Unit = {
+    json match {
+      case JsonObject(values) =>
+        sb.append("{")
+        var i = 0
+        values.foreach {
+          case (key, value) =>
+            sb.append("\"")
+            sb.append(key)
+            sb.append("\"")
+            sb.append(":")
+            render(value, sb)
+            i += 1
+            if (i < values.size) {
+              sb.append(",")
+            }
+        }
+        sb.append("}")
+      case JsonArray(values) =>
+        sb.append("[")
+        var i = 0
+        values.foreach { value =>
+          render(value, sb)
+          i += 1
+          if (i < values.length) {
+            sb.append(",")
+          }
+        }
+        sb.append("]")
+      case JsonString(str) =>
+        renderString(str, sb)
+      case JsonNumber(value) =>
+        sb.append(value)
+      case JsonBoolean(value) =>
+        if (value) {
+          sb.append("true")
+        } else {
+          sb.append("false")
+        }
+      case JsonNull =>
+        sb.append("null")
+    }
+  }
+
+  def renderString(value: String, sb: StringBuilder): Unit = {
+    sb.append("\"")
+    var i   = 0
+    val len = value.length
+    while (i < len) {
+      value.charAt(i) match {
+        case '"'  => sb.append("\\\"")
+        case '\\' => sb.append("\\\\")
+        case '\b' => sb.append("\\b")
+        case '\f' => sb.append("\\f")
+        case '\n' => sb.append("\\n")
+        case '\r' => sb.append("\\r")
+        case '\t' => sb.append("\\t")
+        case c =>
+          if (c < ' ') {
+            sb.append("\\u%04x" format c.toInt)
+          } else {
+            sb.append(c)
+          }
+      }
+      i += 1
+    }
+    sb.append("\"")
+  }
+
+}
---
+++ core/src/test/scala/com/github/tanacasino/example/core/SimpleJsonRendererTest.scala
@@ -0,0 +1,33 @@
+package com.github.tanacasino.example.core
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class SimpleJsonRendererTest extends FlatSpec with Matchers {
+
+  "A SimpleJsonRenderer" should "render json" in {
+    val json = Json.obj(
+      "name"      -> Json.str("Tarou"),
+      "age"       -> Json.num(33),
+      "hasFriend" -> Json.bool(true),
+      "tags" -> Json.arr(
+        Json.str("string"),
+        Json.bool(false),
+        Json.num(10.1),
+        Json.num(10),
+        JsonNull,
+        Json.obj(
+          "key1" -> Json.obj(
+            "key1-1" -> Json.str("value1-1")
+          ),
+          "key2" -> Json.num(10000)
+        )
+      )
+    )
+    val expected =
+      """{"name":"Tarou","age":33,"hasFriend":true,"tags":["string",false,10.1,10,null,{"key1":{"key1-1":"value1-1"},"key2":10000}]}"""
+    val actual = SimpleJsonRenderer.render(json)
+    println(actual)
+    actual shouldBe expected
+  }
+
+}
